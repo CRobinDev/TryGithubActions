d2d557522b87a334974e9a73e57065e57a5030f3
Christopher Robin <williamchen1506@gmail.com>
2024-05-15 20:43:46 +0700

    test

--- main.go
+++ main.go
@@ -3,23 +3,41 @@ package main
 import (
 	"API/controllers"
 	"API/initializers"
+	"log"
+	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func init() {
-	initializers.LoadEnvVariabels()
 	initializers.ConnectToDB()
 }
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Printf("Starting server on %s:%s...\n", os.Getenv("HOST"), port)
+	err = http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+	
 	r := gin.Default()
 	r.GET("/ping", gin.HandlerFunc(func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "ney",
 		})
 	}))
-	
 
 	r.PUT("/posts/:id", controllers.PostsUpdate)
 	r.GET("/posts", controllers.PostsIndex)
